9263a5eb779b359faa199fcf57e5bb473f9408d1
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-01 15:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function volume_sphere() {
     //Write your code here
+	let btn = document.getElementById("submit");
+
+	btn.addEventListener("click", volumevalue => {
+		var radius = document.getElementById("radius").value;
+		var volume = document.getElementById("volume");
+
+		let nonNegative = new Promise((resolve,reject) => {
+			if(radius>=0){
+				resolve(radius);
+			}
+			else{
+				reject("The radius should not be negative");
+			}
+		}).then(res => {
+			let volumeOfSphere = 4/3 * Math.Pi * Math.pow(radius, 3);
+			console.log(volumeOfSphere);
+		})
+		
+		volume.value = radius;
+	})
+	
   
 } 
 
